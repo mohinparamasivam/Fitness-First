7d76811d5bf809fbb7e7c7986f1901b7409efd90
Rin1226 <rinrin112611@gmail.com>
2023-08-10 11:41:50 +0800

    X-Ray

--- Fitness-First/Fitness-First/Startup.cs
+++ Fitness-First/Fitness-First/Startup.cs
@@ -54,6 +54,8 @@ namespace Fitness_First
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseXRay("FitnessFirst System");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
